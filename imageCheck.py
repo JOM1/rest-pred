e3c019c300b3089abf7d440a56fea808512e79b7
JOM1 <jom9839@gmail.com>
2018-06-25 16:15:19 +0300

    changes preprocess loc

--- imageCheck.py
+++ imageCheck.py
@@ -4,12 +4,9 @@ from boto_client import get_file
 from keras.applications.inception_v3 import InceptionV3
 from keras.models import load_model
 from keras.applications.imagenet_utils import decode_predictions
-from keras.preprocessing import image
-import numpy as np
 import tensorflow as tf
-import os, sys, io
-from PIL import Image
-
+import os, sys
+from Image_preprocessor.image_preprocessor import preprocess
 
 MODEL_NAME='model.h5'
 MODEL_PATH='model.h5'
@@ -26,22 +23,12 @@ model = load_model(MODEL_NAME)
 # to fix a bug using avital's soulution https://github.com/keras-team/keras/issues/2397
 graph = tf.get_default_graph()
 
-def proccess_image(img):
-
-    img = Image.open(io.BytesIO(img.read()))
-    img = img.resize((299,299))
-    img_tensor = image.img_to_array(img)                    # (height, width, channels)
-    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
-    img_tensor /= 255.                                      # makes values in the range [0, 1]
-
-    return img_tensor
-
 class ImagePred(Resource):
     def get(self, bucket_name, object_name):
         global graph
         with graph.as_default():
             s3_img = get_file(bucket_name, object_name)
-            classes = model.predict(proccess_image(s3_img))
+            classes = model.predict(preprocess(s3_img))
             top_pred = decode_predictions(classes)[0][0]
             return '{}, score: {}%'.format(top_pred[1], int(top_pred[2]*100))
 
